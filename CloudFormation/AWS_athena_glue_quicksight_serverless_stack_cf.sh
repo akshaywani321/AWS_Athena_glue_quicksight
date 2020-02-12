9bddd842f1fde21590321c9449fa6ba71e70dc14
akshaywani321 <akshaywani321@gmail.com>
2020-02-12 15:46:25 -0500

    bash script

---
+++ CloudFormation/AWS_athena_glue_quicksight_serverless_stack_cf.sh
@@ -0,0 +1,5 @@
+
+aws cloudformation create-stack \
+    --stack-name smart-hub-serverless-stack \
+    --template-body file://./CloudFormation/AWS_athena_glue_quicksight_serverless_stack_cf.json \
+    --capabilities CAPABILITY_NAMED_IAM
--- CloudFormation/AWS_athena_glue_quicksight_stack_cf.sh
+++ CloudFormation/AWS_athena_glue_quicksight_stack_cf.sh
@@ -5,8 +5,8 @@ SCRIPT_BUCKET="smart-hub-scripts-${BUCKET_SUFFIX}"
 LOG_BUCKET="smart-hub-logs-${BUCKET_SUFFIX}"
 
 aws cloudformation create-stack \
-    --stack-name smart-hub-athena-glue-stack \
-    --template-body file://./AWS_athena_glue_quicksight_stack_cf.json\
+    --stack-name smart-hub-application-stack \
+    --template-body file://./CloudFormation/AWS_athena_glue_quicksight_stack_cf.json\
     --parameters ParameterKey=DataBucketName,ParameterValue=${DATA_BUCKET} \
                  ParameterKey=ScriptBucketName,ParameterValue=${SCRIPT_BUCKET} \
                  ParameterKey=LogBucketName,ParameterValue=${LOG_BUCKET} \
