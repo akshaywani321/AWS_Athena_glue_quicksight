c27d5b15e157f05c5ff7765f89f097c50ca23008
akshaywani321 <akshaywani321@gmail.com>
2020-02-12 20:51:26 -0500

    cd

--- CloudFormation/AWS_athena_glue_quicksight_serverless_stack_cf.sh
+++ CloudFormation/AWS_athena_glue_quicksight_serverless_stack_cf.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 aws cloudformation create-stack \
     --stack-name smart-hub-serverless-stack \
     --template-body file://./CloudFormation/AWS_athena_glue_quicksight_serverless_stack_cf.json \
